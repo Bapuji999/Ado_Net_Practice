1a09af115b04e16e226682f68af34df1139e21be
bapuji <babuji.cybrain@gmail.com>
2023-08-31 12:22:30 +0530

    adding connection string

---
+++ ConnectionPracticeInADO.Net/Controllers/TestConnectionString.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConnectionPracticeInADO.Net.Controllers
+{
+    [ApiController]
+    public class TestConnectionString : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public TestConnectionString(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        [Route("CheckConnection")]
+        public string CheckConnection()
+        {
+            DataLayer Dl = new DataLayer(_configuration);
+            string msg = Dl.Getdata();
+            return msg;
+        }
+    }
+}
---
+++ ConnectionPracticeInADO.Net/DataLayer.cs
@@ -0,0 +1,49 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+
+namespace ConnectionPracticeInADO.Net
+{
+    public class DataLayer
+    {
+        private readonly IConfiguration _configuration;
+
+        public DataLayer(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string Getdata()
+        {
+            try
+            {
+                SqlConnection conn = null;
+                string msg = "";
+                using (conn = new SqlConnection(_configuration.GetConnectionString("LocalConnection")))
+                {
+                    conn.Open();
+                    if(conn.State == ConnectionState.Open)
+                    {
+                         msg = msg + "Connected";
+                    }
+                    else
+                    {
+                        return "not Open";
+                    }
+                }
+                if (conn.State == ConnectionState.Open)
+                {
+                    msg = msg + " Still Opend";
+                }
+                else
+                {
+                    msg = msg + " and Closed";
+                }
+                return msg;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
